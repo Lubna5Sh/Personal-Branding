45f028ec68efdc5c231925d66eea80c35b06bd7e
Lubna5Sh <eaa24las@students.eaaa.dk>
2025-02-20 22:21:20 +0000

    Tilføjet billeder + andre ændringer

--- kunst.js
+++ kunst.js
@@ -1,1 +1,13 @@
+const video = document.getElementById("myVideo");
+const playPauseBtn = document.getElementById("playPauseBtn");
+
+playPauseBtn.addEventListener("click", function() {
+    if (video.paused) {
+        video.play();
+        playPauseBtn.textContent = "Pause";
+    } else {
+        video.pause();
+        playPauseBtn.textContent = "Afspil";
+    }
+});
 
